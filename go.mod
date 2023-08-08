64dc3674128c5ccee402427cae3e5731fca5378a
Erik S <erik.sultanaliev@valor-software.com>
2023-08-08 18:23:59 +0600

    Add a function 'getNetworkStatus'

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/ersul4ik/network-checker-bot
 
 go 1.19
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+require (
+	github.com/go-ping/ping v1.1.0 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,92 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-ping/ping"
 	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var welcomeMessage = "Welcome to Network Checker Bot. Please send 'check' to start network checking "
+var byeMessage = "Come again"
+var beginCheckMessage = "Please write the URL that you want to check (www.google.com)"
+
+var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Start checking", "/check"),
+		tgbotapi.NewInlineKeyboardButtonData("Exit", "/exit"),
+	),
+)
+
+func getNetworkStatus(url string) string {
+	pinger, err := ping.NewPinger(url)
+	if err != nil {
+		log.Println("\n PANIC")
+		panic(err)
+	}
+	pinger.Count = 3
+	if err := pinger.Run(); err != nil {
+		log.Panic(err)
+	}
+
+	stats := pinger.Statistics()
+	return fmt.Sprintf("Percentage of packets lost (%s): %f", url, stats.PacketLoss)
+}
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
 	if err != nil {
 		panic(err)
 	}
-
 	bot.Debug = true
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 10
+
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.CallbackQuery != nil {
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
+			switch update.CallbackQuery.Data {
+			case "/check":
+				msg.Text = beginCheckMessage
+				go func() {
+					status := getNetworkStatus("www.google.com")
+					msg.Text = status
+					_, err := bot.Send(msg)
+					if err != nil {
+						log.Panic(err)
+					}
+				}()
+			case "/exit":
+				msg.Text = byeMessage
+				if _, err := bot.Send(msg); err != nil {
+					log.Panic(err)
+				}
+			}
+			continue
+		}
+
+		if update.Message == nil { // ignore non-Message updates
+			log.Printf("msg is empty")
+			continue
+		}
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "start":
+				msg.Text = welcomeMessage
+				msg.ReplyMarkup = numericKeyboard
+			default:
+				msg.Text = "I don't know that command"
+			}
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+			continue
+		}
+	}
 }
